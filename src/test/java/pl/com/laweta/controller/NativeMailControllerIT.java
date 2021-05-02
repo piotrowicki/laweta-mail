18cd7c3fca416bbe27c980081f59573e647056b9
Piotr Nowicki <piotrowicki@gmail.com>
2021-05-02 10:42:56 +0200

    Remove native test

--- src/test/java/pl/com/laweta/controller/NativeMailControllerIT.java
+++
@@ -1,9 +0,0 @@
-package pl.com.laweta.controller;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMailControllerIT extends LawetaMailTest {
-
-    // Execute the same tests but in native mode.
-}
